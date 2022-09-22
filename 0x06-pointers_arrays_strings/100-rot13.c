564c4be8b71cd5e6a7ed42fbb39faae7da41cd49
Ndman99 <nprico99@gmail.com>
2022-09-22 13:11:49 -0700

    a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ */
+
+char *rot13(char *str)
+{
+	int indx1 = 0, indx2;
+	char alphabet[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13key[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 < 52; indx2++)
+		{
+			if (str[indx1] == alphabet[indx2])
+			{
+				str[indx1] = rot13key[indx2];
+				break;
+			}
+		}
+
+		indx1++;
+	}
+
+	return (str);
+}
